0abbfed34a9b10cd4eb2dd3646aa6ddeb743eaf5
李英阔 <939855973@qq.com>
2018-09-19 00:21:38 +0800

    仿抖音视频选取音乐时截取音频的长度

---
+++ AudioCutView/app/src/main/java/com/kkcast/audiocutview/AudioCutView.java
@@ -0,0 +1,150 @@
+package com.kkcast.audiocutview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+import java.util.Random;
+
+
+/**
+ * @author kcast
+ * @date 2018/09/19
+ */
+
+public class AudioCutView extends View {
+
+    private  Paint paint;
+
+    private float maxProgress=100.0f;
+
+    /**
+     * 当前进度，100进制的
+     */
+    private float currentProgress=0.0f;
+
+    private float viewCurrentProgress=0;
+
+    private int measuredHeight;
+    private int measuredWidth;
+
+    private float videoProgressWidth=100;
+
+    private  Paint progressPaint;
+
+    private float videoWidthRate;
+
+    private Paint wavePaint;
+
+
+    private float[] smoothedGains;
+
+
+
+    public AudioCutView(Context context, @Nullable AttributeSet attrs) {
+
+        super(context, attrs);
+        paint = new Paint();
+        progressPaint = new Paint();
+        progressPaint.setAntiAlias(true);
+        progressPaint.setStyle(Paint.Style.STROKE);
+
+        wavePaint = new Paint();
+        wavePaint.setColor(Color.parseColor("#FF7701"));
+        wavePaint.setStrokeWidth(dpToPx(3));
+        wavePaint.setAntiAlias(false);
+
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+        measuredHeight = getMeasuredHeight();
+        measuredWidth = getMeasuredWidth();
+
+        computeSmoothedGains();
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+            int width = getWidth();
+            int height = getHeight();
+            float ctr = height / 2f;
+            for (int i = 0; i < width; i+=15) {
+                canvas.drawLine(i, ctr-smoothedGains[i]/2, i, ctr+smoothedGains[i]/2, wavePaint);
+        }
+
+
+
+        videoProgressWidth=measuredWidth*videoWidthRate;
+        paint.setStrokeWidth(getHeight());
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+//        paint.setColor(Color.parseColor("#87654321"));
+
+//        canvas.drawLine(0,measuredHeight/2,measuredWidth,measuredHeight/2,paint);
+
+        paint.setColor(Color.parseColor("#87654321"));
+
+        viewCurrentProgress=(measuredWidth-videoProgressWidth)*currentProgress/maxProgress;
+        canvas.drawLine(viewCurrentProgress,measuredHeight/2,viewCurrentProgress+videoProgressWidth,measuredHeight/2,paint);
+
+    }
+
+
+    public void setVideoProgressWidth(float videoWidthRate){
+        this.videoWidthRate=videoWidthRate;
+    }
+
+
+    /**
+     * 设置最大进度
+     * @param maxProgress
+     */
+    public void setMaxProgress(float maxProgress){
+        this.maxProgress=maxProgress;
+    }
+
+
+    /**
+     * 设置当前进度
+     * @param currentNum
+     */
+    public void setCurrentProgress(float currentNum){
+        currentProgress=currentNum;
+        postInvalidate();
+    }
+
+
+
+    public void computeSmoothedGains() {
+
+        smoothedGains = new float[measuredWidth];
+        Random rand = new Random();
+        int MAX=180;
+        int MIN=80;
+        for (int i=0;i<measuredWidth;i++){
+            int randNumber =rand.nextInt(MAX - MIN + 1) + MIN;
+            smoothedGains[i]=randNumber;
+        }
+
+        postInvalidate();
+    }
+
+    public float dpToPx(float dp) {
+
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+
+
+}
--- AudioCutView/app/src/main/java/com/kkcast/audiocutview/MainActivity.java
+++ AudioCutView/app/src/main/java/com/kkcast/audiocutview/MainActivity.java
@@ -2,12 +2,45 @@ package com.kkcast.audiocutview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AudioCutView audio_cut_view;
+    private SeekBar seekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+        audio_cut_view = findViewById(R.id.audio_cut_view);
+        seekbar = findViewById(R.id.seekbar);
+
+
+        audio_cut_view.setVideoProgressWidth(0.3f);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audio_cut_view.setCurrentProgress(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 }
